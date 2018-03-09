d58a19efd29da8f1f5e5aeb8cfa8c261370abbc3
Abe _King_ <29788070+abe-noll@users.noreply.github.com>
2018-03-08 19:21:43 -0700

    Java code now returns metrics

--- native/AndroidDisplayUtility.java
+++ native/AndroidDisplayUtility.java
@@ -6,15 +6,11 @@ import android.view.WindowManager;
 public class AndroidDisplayUtility {
     private static final String TAG = "AndroidDisplayUtility";
     
-    static public float getDPI( boolean vertical ) {
+    static public DisplayMetrics getMetrics(){
 	    DisplayMetrics metrics = new DisplayMetrics();
         Monkey2Activity.mSingleton.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         
-        if( vertical ){
-            return metrics.ydpi;
-        } else {
-            return metrics.xdpi;
-        }
+        return metrics;
     }
     
 }
